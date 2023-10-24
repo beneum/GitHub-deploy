a7d005d9ac105d7d748532a8bf88ee55fc84984e
taeheon <beneum11@gmail.com>
2023-10-24 22:21:18 +0900

    return to original state

--- src/components/MainSkills.js
+++ src/components/MainSkills.js
@@ -7,18 +7,18 @@ return (
 <div>
     <h2 className={`${styles.MainSkills}`} ref={ref}>Main Skills</h2>
     <div className={`${styles.main_skills_img}`}>
-        <img src={`${process.env.PUBLIC_URL}`+`/htmlcssjs2.png`} alt="" />
-        <img src={`${process.env.PUBLIC_URL}`+`/react3.png`} alt="" />
-        <img src={`${process.env.PUBLIC_URL}`+`/redux2.png`} alt="" />
-        <img src={`${process.env.PUBLIC_URL}`+`/Recoil.png`} alt="" />
-        <img src={`${process.env.PUBLIC_URL}`+`/ts.png`} alt="" />
-        <img src={`${process.env.PUBLIC_URL}`+`/sass.png`} alt="" />
-        <img src={`${process.env.PUBLIC_URL}`+`/github.png`} alt="" />
-        <img src={`${process.env.PUBLIC_URL}`+`/jquery.png`} alt="" />
-        <img src={`${process.env.PUBLIC_URL}`+`/git.png`} alt="" />
-        <img src={`${process.env.PUBLIC_URL}`+`/Firebase.png`} alt="" />
-        <img src={`${process.env.PUBLIC_URL}`+`/tailwind.png`} alt="" />
-        <img src={`${process.env.PUBLIC_URL}`+`/vercel.png`} alt="" />
+        <img src={`${process.env.PUBLIC_URL}/htmlcssjs2.png`} alt="" />
+        <img src={`${process.env.PUBLIC_URL}/react3.png`} alt="" />
+        <img src={`${process.env.PUBLIC_URL}/redux2.png`} alt="" />
+        <img src={`${process.env.PUBLIC_URL}/Recoil.png`} alt="" />
+        <img src={`${process.env.PUBLIC_URL}/ts.png`} alt="" />
+        <img src={`${process.env.PUBLIC_URL}/sass.png`} alt="" />
+        <img src={`${process.env.PUBLIC_URL}/github.png`} alt="" />
+        <img src={`${process.env.PUBLIC_URL}/jquery.png`} alt="" />
+        <img src={`${process.env.PUBLIC_URL}/git.png`} alt="" />
+        <img src={`${process.env.PUBLIC_URL}/Firebase.png`} alt="" />
+        <img src={`${process.env.PUBLIC_URL}/tailwind.png`} alt="" />
+        <img src={`${process.env.PUBLIC_URL}/vercel.png`} alt="" />
     </div>
 </div>
 )
